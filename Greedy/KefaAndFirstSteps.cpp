0bbcf80b5bfb54626e0e998c337e88889fef7628
Miguel Flores <abraham.flores4433@alumnos.udg.mx>
2023-04-27 14:34:32 -0500

    27/04/23

---
+++ Greedy/KefaAndFirstSteps.cpp
@@ -0,0 +1,44 @@
+/*
+https://codeforces.com/problemset/problem/580/A
+                VOLUME: ▁▂▃▄▅▆▇ 100%
+                SONG: 4th Dimension♫
+                CREATOR: KIDS SEE GHOSTS™®
+                0:35 ━❍──────── -3:56
+                ↻     ⊲  Ⅱ  ⊳     ↺
+                   
+*/
+//g++ -std=c++17 a.cpp && a < in.txt > out.txt
+#include<bits/stdc++.h>
+#define fore(i,a,b) for(int i=(a), FINALD=(b); i<FINALD; ++i)
+#define ENDL '\n'
+#define f first
+#define s second
+#define pb push_back
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define sz(s) int(s.size())
+#define all(s) s.begin(), s.end()
+#define yes cout<<"YES"<<ENDL;
+#define nop cout<<"NO"<<ENDL;
+using namespace std;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef pair<int, int> pi;
+typedef vector<lli> vi;
+
+int main(){_
+    lli n; cin>>n;
+    vi nums (n);
+    vi anss;
+    fore(i,0,n){cin>>nums[i];}
+    lli aux=0, ans=0;
+    for(auto e:nums){
+        if(e>=aux){ans++;}
+        else{anss.pb(ans); ans=1;}
+        aux=e; 
+    }
+    anss.pb(ans);
+    sort(anss.rbegin(), anss.rend());
+    cout<<anss[0]<<ENDL;
+
+    return 0;
+}
