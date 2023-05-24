f51ffee40f78f92548e1e87a43901d47d0825a7e
Miguel Flores <abraham.flores4433@alumnos.udg.mx>
2023-05-24 17:07:29 -0500

    24/05/23

---
+++ 2022 ICPC Gran Premio de Mexico 1ra Fecha/h.cpp
@@ -0,0 +1,40 @@
+/*
+
+                VOLUME: ▁▂▃▄▅▆▇ 100%
+                SONG: 4th Dimension♫
+                CREATOR: KIDS SEE GHOSTS™®
+                0:35 ━❍──────── -3:56
+                ↻     ⊲  Ⅱ  ⊳     ↺
+                   
+*/
+//g++ -std=c++17 a.cpp && a < in.txt > out.txt
+#include<bits/stdc++.h>
+#define fore(i,a,b) for(int i=(a), FINALD=(b); i<FINALD; ++i)
+#define ENDL '\n'
+#define f first
+#define s second
+#define pb push_back
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define sz(s) int(s.size())
+#define all(s) s.begin(), s.end()
+#define yes cout<<"YES"<<ENDL;
+#define nop cout<<"NO"<<ENDL;
+using namespace std;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef pair<int, int> pi;
+typedef vector<lli> vi;
+
+int main(){_
+    int n, aux, ans=0; cin>>n;
+    aux=n/4;
+    if((n%4) != 0){
+        int aux2=n-(aux*2);
+        aux2=aux2/2;
+        ans=aux*aux2;
+    }else{
+        ans=aux*aux;
+    }
+    cout<<ans<<ENDL;
+    return 0;
+}
