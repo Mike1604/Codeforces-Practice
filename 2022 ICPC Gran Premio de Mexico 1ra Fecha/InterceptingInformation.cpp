ba3b20ce402c1e1a97105ec2f12cbf55528cdd5b
Miguel Flores <abraham.flores4433@alumnos.udg.mx>
2023-05-26 10:09:07 -0500

    Create InterceptingInformation.cpp

---
+++ 2022 ICPC Gran Premio de Mexico 1ra Fecha/InterceptingInformation.cpp
@@ -0,0 +1,39 @@
+/*
+
+                VOLUME: ▁▂▃▄▅▆▇ 100%
+                SONG: 4th Dimension♫
+                CREATOR: KIDS SEE GHOSTS™®
+                0:35 ━❍──────── -3:56
+                ↻     ⊲  Ⅱ  ⊳     ↺
+                   
+*/
+//g++ -std=c++17 a.cpp && a < in.txt > out.txt
+#include<bits/stdc++.h>
+#define fore(i,a,b) for(int i=(a), FINALD=(b); i<FINALD; ++i)
+#define ENDL '\n'
+#define f first
+#define s second
+#define pb push_back
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define sz(s) int(s.size())
+#define all(s) s.begin(), s.end()
+#define yes cout<<"YES"<<ENDL;
+#define nop cout<<"NO"<<ENDL;
+using namespace std;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef pair<int, int> pi;
+typedef vector<lli> vi;
+
+int main(){_
+    bool flag=true;
+    fore(i,0,8){
+        int n; cin>>n;
+        if((n != 0) && (n != 1)){
+            flag=false;
+        }
+    }
+    if(flag){cout<<"S"<<ENDL;}
+    else{cout<<"F"<<ENDL;}
+    return 0;
+}
